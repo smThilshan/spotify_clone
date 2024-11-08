1be12709e08a39ef387ae1e1aba2e8f2c0d48ddd
Thilshan Mohamed <thilshanmohamed@Thilshans-MacBook-Pro.local>
2024-11-08 11:03:10 +0530

    initial commit

---
+++ lib/core/config/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/config/theme/app_colors.dart';
+
+class AppTheme {
+  static final lighTheme = ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      brightness: Brightness.light,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
+
+  static final darkTheme = ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      brightness: Brightness.dark,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
+}
